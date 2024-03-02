f0e52b4f130adbd110c0d97e2d81b7b5c467572e
rahulsahay19 <rahulsahay19@hotmail.com>
2024-03-02 21:11:16 +0530

    Added Validation for address page

--- client/src/features/checkout/AddressForm.tsx
+++ client/src/features/checkout/AddressForm.tsx
@@ -1,104 +1,123 @@
-import { Typography, Grid, TextField, FormControlLabel, Checkbox } from "@mui/material";
+import {
+  Typography,
+  Grid,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+import { useFormContext } from "react-hook-form";
 
 export default function AddressForm() {
+  const { register, formState: { errors } } = useFormContext(); // Access form context
+
   return (
     <>
       <Typography variant="h6" gutterBottom>
         Shipping address
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="firstName"
-            name="firstName"
-            label="First name"
-            fullWidth
-            autoComplete="given-name"
-            variant="standard"
-          />
+      <form>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="firstName"
+              {...register("firstName", { shouldUnregister: false })}
+              label="First name"
+              fullWidth
+              autoComplete="given-name"
+              variant="standard"
+              error={!!errors.firstName} 
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="lastName"
+              {...register("lastName")}
+              label="Last name"
+              fullWidth
+              autoComplete="family-name"
+              variant="standard"
+              error={!!errors.lastName}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="address1"
+              {...register("address1")}
+              label="Address line 1"
+              fullWidth
+              autoComplete="shipping address-line1"
+              variant="standard"
+              error={!!errors.address1}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="address2"
+              {...register("address2")}
+              label="Address line 2"
+              fullWidth
+              autoComplete="shipping address-line2"
+              variant="standard"
+              error={!!errors.address2}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="city"
+              {...register("city")}
+              label="City"
+              fullWidth
+              autoComplete="shipping address-level2"
+              variant="standard"
+              error={!!errors.city}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="state"
+              {...register("state")}
+              label="State/Province/Region"
+              fullWidth
+              variant="standard"
+              error={!!errors.state}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="zip"
+              {...register("zip")}
+              label="Zip / Postal code"
+              fullWidth
+              autoComplete="shipping postal-code"
+              variant="standard"
+              error={!!errors.zip}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="country"
+              {...register("country")}
+              label="Country"
+              fullWidth
+              autoComplete="shipping country"
+              variant="standard"
+              error={!!errors.country}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={<Checkbox color="secondary" {...register("saveAddress")} />}
+              label="Use this address for payment details"
+            />
+          </Grid>          
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="lastName"
-            name="lastName"
-            label="Last name"
-            fullWidth
-            autoComplete="family-name"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="address1"
-            name="address1"
-            label="Address line 1"
-            fullWidth
-            autoComplete="shipping address-line1"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="address2"
-            name="address2"
-            label="Address line 2"
-            fullWidth
-            autoComplete="shipping address-line2"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="shipping address-level2"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="state"
-            name="state"
-            label="State/Province/Region"
-            fullWidth
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="zip"
-            name="zip"
-            label="Zip / Postal code"
-            fullWidth
-            autoComplete="shipping postal-code"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="country"
-            name="country"
-            label="Country"
-            fullWidth
-            autoComplete="shipping country"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-            label="Use this address for payment details"
-          />
-        </Grid>
-      </Grid>
+      </form>
     </>
   );
 }
---
+++ client/src/features/checkout/checkoutValidation.ts
@@ -0,0 +1,12 @@
+import * as yup from 'yup';
+
+export const validationRules = yup.object({
+    firstName: yup.string().required('Full Name is required'),
+    lastName: yup.string().required('Last Name is required'),
+    address1: yup.string().required('Address1 is required'),
+    address2: yup.string().optional(),
+    city: yup.string().required('City is required'),
+    state: yup.string().required('State is required'),
+    zip: yup.string().required('Zip is required'),
+    country: yup.string().required('Country is required')
+})
